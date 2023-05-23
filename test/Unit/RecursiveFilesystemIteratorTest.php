031d5fd794f0bce12a1682321f83cfefa36f2fc8
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2023-05-23 18:39:21 +0300

    Fix recurisve iterator tests

--- test/Unit/RecursiveFilesystemIteratorTest.php
+++ test/Unit/RecursiveFilesystemIteratorTest.php
@@ -19,13 +19,13 @@ class RecursiveFilesystemIteratorTest extends TestCase
 
     public function testIteratorScansAllDirectories()
     {
-        $iterator = new RecursiveFilesystemIterator(dirname(__DIR__) . '/input');
+        $iterator = new RecursiveFilesystemIterator(dirname(__DIR__) . '/input/recursive');
         $result = [];
         $expected = [
-            'input/dir1',
-            'input/dir1/file1_in_dir1',
-            'input/dir2',
-            'input/dir3',
+            'recursive/dir1',
+            'recursive/dir1/file1_in_dir1',
+            'recursive/dir2',
+            'recursive/dir3',
         ];
 
         foreach($iterator as $file) {
@@ -38,18 +38,18 @@ class RecursiveFilesystemIteratorTest extends TestCase
 
     public function testIteratorCanSkipDirectories()
     {
-        $iterator = new RecursiveFilesystemIterator(dirname(__DIR__) . '/input');
+        $iterator = new RecursiveFilesystemIterator(dirname(__DIR__) . '/input/recursive');
         $result = [];
         $expected = [
-            'input/dir2',
-            'input/dir3',
+            'recursive/dir2',
+            'recursive/dir3',
         ];
 
         foreach($iterator as $file) {
 
             $relativePath = $file->getRelativePath(dirname($iterator->getPath()));
 
-            if($relativePath === 'input/dir1') {
+            if($relativePath === 'recursive/dir1') {
                 $iterator->skipDirectory();
                 continue;
             }
