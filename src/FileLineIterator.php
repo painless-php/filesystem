93ae7be0c75dae5079f5d4c0047940475936d689
Jyri Mikkola <jyri.mikkola@pp.inet.fi>
2023-05-23 20:30:39 +0300

    Refactor FileLineIterator

--- src/FileLineIterator.php
+++ src/FileLineIterator.php
@@ -7,13 +7,17 @@ use SeekableIterator;
 
 class FileLineIterator implements SeekableIterator
 {
-    private $file;
-    private $stream;
-    private $currentLineNumber;
+    private File $file;
+    private mixed $stream;
+    private int $currentLineNumber;
+    private bool $readEmptyLines;
+    private bool $stripLineEndings; // TODO
 
-    public function __construct(File $file)
+    public function __construct(File|string $file, $readEmptyLines = false, bool $stripLineEndings = true)
     {
-        $this->file = $file;
+        $this->file = $file instanceof File ? $file : new File($file);
+        $this->readEmptyLines = $readEmptyLines;
+        $this->stripLineEndings = $stripLineEndings;
         $this->currentLineNumber = 0;
     }
 
@@ -25,105 +29,24 @@ class FileLineIterator implements SeekableIterator
         }
     }
 
-    /**
-     * Read lines using user supplied callback function.
-     *
-     * @param callable $cb Read callback function. Return false to break the
-     * read loop.
-     *
-     * @param bool $readBlank If set to true, lines with only whitespace will
-     * also be red.
-     *
-     * @param bool $stripLineEndings set to true if you want to remove line
-     * endings for the returned lines.
-     *
-     */
-    public function read(callable $cb, bool $readBlank = false, bool $stripLineEndings = true)
-    {
-        $lineNumber = 1;
-
-        foreach($this as $line) {
-            /* Skip empty lines when readBlank argument is not used */
-            if(! $readBlank && trim($line) === '') {
-                continue;
-            }
-
-            /* Strip line ending if option is in use */
-            if($stripLineEndings && str_ends_with($line, PHP_EOL)) {
-                $line = substr($line, 0, strlen($line) - strlen(PHP_EOL));
-            }
-
-            /* Return line content and index to callback */
-            if($cb($line, $lineNumber) === false) {
-                break;
-            }
-
-            $lineNumber++;
-        }
-    }
-
-    /**
-     * Get lines.
-     *
-     * @param int $offset How many lines should be skipped from the beginning
-     * of the file.
-     *
-     * @param int|null $limit How many lines should be returned.
-     *
-     * @param bool $readBlank Set to false if you want to skip lines with only
-     * whitespace.
-     *
-     * @param bool $stripLineEndings set to true if you want to remove line
-     * endings for the returned lines.
-     *
-     * @return array
-     */
-    public function get(int $offset = 0, ?int $limit = null, bool $readBlank = false, bool $stripLineEndings = true) : array
-    {
-        $lines = [];
-        $this->read(static function($line, $lineNumber) use ($offset, &$lines, $limit) {
-
-            /* Only capture lines after offset */
-            if($lineNumber > $offset) {
-                $lines[] = $line;
-            }
-
-            /* Stop reading after limit */
-            if($limit !== null && count($lines) >= $limit) {
-                return false;
-            }
-        }, $readBlank, $stripLineEndings);
-
-        return $lines;
-    }
-
-    public function count(bool $countEmpty = false) : int
+    public function count() : int
     {
         $lineCount = 0;
 
         foreach($this as $line) {
             /* Skip empty lines when countEmpty argument is not used */
-            if(! $countEmpty && trim($line) === '') {
+            if(! $this->readEmptyLines && trim($line) === '') {
                 continue;
             }
             $lineCount++;
         }
 
         /* Remove one from line count to account for feof operation */
-        if($countEmpty) {
+        if($this->readEmptyLines) {
             $lineCount--;
         }
 
         return $lineCount;
-    }
-
-    private function getStream()
-    {
-        if($this->stream === null) {
-            $this->stream = $this->file->openStream(FilesystemStreamMode::Read);
-        }
-
-        return $this->stream;
     }
 
     public function seek(int $offset) : void
@@ -171,5 +94,14 @@ class FileLineIterator implements SeekableIterator
     public function getFile() : File
     {
         return $this->file;
+    }
+
+    private function getStream()
+    {
+        if($this->stream === null) {
+            $this->stream = $this->file->openStream(FilesystemStreamMode::Read);
+        }
+
+        return $this->stream;
     }
 }
